4c543ba273e01a2dff109a710743fc38e95bdc29
seanlabrador0625 <2022-02459@sanbeda.edu.ph>
2024-10-29 15:43:07 +0800

    modified

--- application/views/admin/AdminUserAccounts/editUserAccount.php
+++ application/views/admin/AdminUserAccounts/editUserAccount.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Bedan Travel & Tours</title>
 
 	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/adminStyle.css?<?php echo time(); ?>"> 
 
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="navbar-left">
 			<nav>
 				<div class="logo-container">
-					<img id="logo" src="http://localhost/travelagency_ci/images/logo/web_logo.png" alt="">
+					<img id="logo" src="http://localhost/GitHub/travelagency/images/logo/web_logo.png" alt="">
 				</div>
 
 				<div class="NavPic-container">
@@ -40,29 +40,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 				<div class="sidebar">
 					<ul>
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showAdminDashboard">
-							<img src="http://localhost/travelagency_ci/images/icons/dashboard.png" alt="Dashboard-icon"><span>Dashboard</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showAdminDashboard">
+							<img src="http://localhost/GitHub/travelagency/images/icons/dashboard.png" alt="Dashboard-icon"><span>Dashboard</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showUserAccounts">
-							<img src="http://localhost/travelagency_ci/images/icons/userAccounts.png" alt="User-Accounts-icons"><span>User Accounts</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showUserAccounts">
+							<img src="http://localhost/GitHub/travelagency/images/icons/userAccounts.png" alt="User-Accounts-icons"><span>User Accounts</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showTourPackages">
-							<img src="http://localhost/travelagency_ci/images/icons/tourPackages.png" alt="Tour-Packages-icon"><span>Tour Packages</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showTourPackages">
+							<img src="http://localhost/GitHub/travelagency/images/icons/tourPackages.png" alt="Tour-Packages-icon"><span>Tour Packages</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showAdminBookings">
-							<img src="http://localhost/travelagency_ci/images/icons/bookings.png" alt="Bookings-icon"><span>Bookings</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showAdminBookings">
+							<img src="http://localhost/GitHub/travelagency/images/icons/bookings.png" alt="Bookings-icon"><span>Bookings</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showAdminArchives">
-							<img src="http://localhost/travelagency_ci/images/icons/archives.png" alt="Archives-icon"><span>Archives</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showAdminArchives">
+							<img src="http://localhost/GitHub/travelagency/images/icons/archives.png" alt="Archives-icon"><span>Archives</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showTransactions">
-							<img src="http://localhost/travelagency_ci/images/icons/transactions.png" alt="Transactions-icon"></span>Transactions</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showTransactions">
+							<img src="http://localhost/GitHub/travelagency/images/icons/transactions.png" alt="Transactions-icon"></span>Transactions</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/AdminController/showAdminReviews">
-							<img src="http://localhost/travelagency_ci/images/icons/reviews.png" alt="Reviews-icon"><span>Reviews</span></a></li>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/AdminController/showAdminReviews">
+							<img src="http://localhost/GitHub/travelagency/images/icons/reviews.png" alt="Reviews-icon"><span>Reviews</span></a></li>
 
-						<li><a href="http://localhost/travelagency_ci/index.php/Account/LogOut">
-							<img src="http://localhost/travelagency_ci/images/icons/logout.png" alt="LogOut-icon"><span>Log Out</span></li></a>
+						<li><a href="http://localhost/GitHub/travelagency/index.php/Account/LogOut">
+							<img src="http://localhost/GitHub/travelagency/images/icons/logout.png" alt="LogOut-icon"><span>Log Out</span></li></a>
 					</ul>
 				</div>
 			</nav>
