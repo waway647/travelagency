d3719459fe73e751829033df2ac48a50a7870bdb
seanlabrador0625 <2022-02459@sanbeda.edu.ph>
2024-10-31 18:34:46 +0800

    modified

--- application/views/admin/AdminTourPackage/adminTourPackages.php
+++ application/views/admin/AdminTourPackage/adminTourPackages.php
@@ -97,24 +97,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <!-- View Modal -->
                         <div class="modal" id="myModal">
                             <div class="modal-dialog">
-                                <div class="modal-content">
+                                <div class="modal-bg">
+                                    <div class="modal-content">
+                                        <!-- Modal Header -->
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Tour Package Details</h4>
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
 
-                                    <!-- Modal Header -->
-                                    <div class="modal-header">
-                                        <h4 class="modal-title">Tour Package Details</h4>
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <!-- Modal body -->
+                                        <div class="modal-body">
+                                            <!-- Content will be loaded here from JavaScript -->
+                                        </div>
+
+                                        <!-- Modal footer -->
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                        </div>
                                     </div>
-
-                                    <!-- Modal body -->
-                                    <div class="modal-body">
-                                        <!-- Content will be loaded here from JavaScript -->
-                                    </div>
-
-                                    <!-- Modal footer -->
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
