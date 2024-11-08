4d4557a7e76c131652110385f09363be9c37a9d9
seanlabrador0625 <2022-02459@sanbeda.edu.ph>
2024-11-08 16:22:29 +0800

    modified

--- application/config/google.php
+++ application/config/google.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['google']['client_id']        = '144239651874-0keuk9fi648qn79fudjdimfgut4ctm5f.apps.googleusercontent.com';
 $config['google']['client_secret']    = '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';
-$config['google']['redirect_uri']     = 'http://localhost/GitHub/travelagency/index.php/Account/redirect_signIn';
-$config['google']['application_name'] = 'Login to CodexWorld.com';
+$config['google']['redirect_uri']     = 'http://localhost/GitHub/travelagency/index.php/Account/redirect_userHomePage';
+$config['google']['application_name'] = 'Login to Travel Agency';
 $config['google']['api_key']          = '';
 $config['google']['scopes']           = array();
