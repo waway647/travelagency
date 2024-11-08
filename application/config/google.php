a8f4e9efcf1f276e1495d0baf6dbedae7ad79194
seanlabrador0625 <2022-02459@sanbeda.edu.ph>
2024-11-08 16:14:20 +0800

    modified

---
+++ application/config/google.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+|  Google API Configuration
+| -------------------------------------------------------------------
+| 
+| To get API details you have to create a Google Project
+| at Google API Console (https://console.developers.google.com)
+| 
+|  client_id         string   Your Google API Client ID.
+|  client_secret     string   Your Google API Client secret.
+|  redirect_uri      string   URL to redirect back to after login.
+|  application_name  string   Your Google application name.
+|  api_key           string   Developer key.
+|  scopes            string   Specify scopes
+*/
+$config['google']['client_id']        = '144239651874-0keuk9fi648qn79fudjdimfgut4ctm5f.apps.googleusercontent.com';
+$config['google']['client_secret']    = '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';
+$config['google']['redirect_uri']     = 'http://localhost/GitHub/travelagency/index.php/Account/redirect_signIn';
+$config['google']['application_name'] = 'Login to CodexWorld.com';
+$config['google']['api_key']          = '';
+$config['google']['scopes']           = array();
